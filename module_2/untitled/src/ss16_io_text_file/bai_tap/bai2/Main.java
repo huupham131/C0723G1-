a8d6cc0010b88ba08a2c0fb30b43572ca42d4673
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-09-25 14:12:41 +0700

    fixbt16

--- module_2/untitled/src/ss16_io_text_file/bai_tap/bai2/Main.java
+++ module_2/untitled/src/ss16_io_text_file/bai_tap/bai2/Main.java
@@ -1,5 +1,8 @@
 package ss16_io_text_file.bai_tap.bai2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -22,7 +25,25 @@ public class Main {
 //            e.printStackTrace();
 //        }
         // Cach 2:
-
+        try {
+            File file = new File("/Users/macbook/Documents/GitHub/C0723G1-PhamThanhHuu/module_2/untitled/src/ss16_io_text_file/bai_tap/bai2/data.txt");
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line = null;
+            System.out.format("%-5s %-15s %-15s\n", "STT", "CODE", "TEN");
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] dataArr = line.split(",");
+                Country country = new Country();
+                for (String data : dataArr) {
+                   country.setId(Integer.valueOf(dataArr[0]));
+                   country.setCode(dataArr[1]);
+                   country.setName(dataArr[2]);
+                }
+                System.out.format("%-5s %-15s %-15s\n", country.getId(), country.getCode(), country.getName());
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
