2897d63f61de856f4da28a0b0d071f9dadddd18a
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-10-11 10:35:09 +0700

    updatebai1

--- module_3/ss1_database_introduction/Untitled.sql
+++ module_3/ss1_database_introduction/Untitled.sql
@@ -2,7 +2,7 @@ create database demo;
 
 use demo;
 
-create table Student(
+create table student(
  id int,
  name varchar(200),
  age int,
@@ -12,9 +12,13 @@ create table class(
 id int,
 name varchar(200)
 );
-create table Teacher(
+create table teacher(
 id int,
 name varchar(200),
  age int,
  country varchar(50)
 );
+insert into student value(1,"Hữu",25,"Việt Nam");
+select* from student;
+insert into class value(1,"C0723G1");
+insert into teacher value(1,"Huu",25,"VietNam");
