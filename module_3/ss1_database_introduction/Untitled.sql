3a90792ece82b473592fbe1a2f944d4157a0251d
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-10-11 10:19:43 +0700

    updatebt1

---
+++ module_3/ss1_database_introduction/Untitled.sql
@@ -0,0 +1,20 @@
+create database demo;
+
+use demo;
+
+create table Student(
+ id int,
+ name varchar(200),
+ age int,
+ country varchar(50)
+);
+create table class(
+id int,
+name varchar(200)
+);
+create table Teacher(
+id int,
+name varchar(200),
+ age int,
+ country varchar(50)
+);
