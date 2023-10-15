fe26d40688a06d3b81ea519e2e34fa87e7b6c294
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-10-15 20:56:33 +0700

    fix6-10

--- module_3/case_study/q6-q10.sql
+++ module_3/case_study/q6-q10.sql
@@ -4,16 +4,22 @@ use case_study;
 select s.id, s.name_service, s.acreage, s.price, ts.`name` as name_type_service
 from service s
 join type_service ts on s.id_type_service = ts.id
-where s.id not in (select ct.id
+where s.id not in (
+select ct.id_service
 from contract ct
 where (year(ct.start_date) = 2021 and quarter(ct.start_date) = 1 ));
+-- where ct.start_date between 2021-01-01 and 2021-03-31);
 -- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
 -- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
 select s.id, s.name_service, s.acreage , s.max_persons, s.price, ts.`name` as name_type_service
 from service s
-left join type_service ts on s.id_type_service = ts.id
-left join contract ct on s.id = ct.id_service
-where (year(ct.start_date) = 2020 and not year(ct.start_date) = 2021)
+join type_service ts on s.id_type_service = ts.id
+join contract ct on s.id = ct.id_service
+where year(ct.start_date) = 2020 
+and s.id not in(
+select id_service
+from contract
+where year(start_date) = 2021)
 group by s.id;
 -- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
 -- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
