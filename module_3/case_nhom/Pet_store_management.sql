8488439287ce05f6d3343a2fb72b16dcc27245ab
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-11-02 22:06:17 +0700

    Update Pet_store_management.sql

--- module_3/case_nhom/Pet_store_management.sql
+++ module_3/case_nhom/Pet_store_management.sql
@@ -85,3 +85,32 @@ quantity int default 0,
 foreign key (id_service) references service(id_service),
 foreign key (id_booking) references booking(id_booking)
 );
+-- thêm dữ liệu test
+INSERT INTO roles (roles) VALUES ('Admin');
+INSERT INTO roles (roles) VALUES ('Customer');
+INSERT INTO accounts (username, `password`, id_role) VALUES ('admin_user', 'admin_password', 1);
+INSERT INTO accounts (username, `password`, id_role) VALUES ('customer_user', 'customer_password', 2);
+INSERT INTO type_customer (name_type_customer) VALUES ('Regular');
+INSERT INTO type_customer (name_type_customer) VALUES ('VIP');
+INSERT INTO type_pet (name_type_pet) VALUES ('Dog');
+INSERT INTO type_pet (name_type_pet) VALUES ('Cat');
+INSERT INTO employees (employee_name, birthday, phone_number, address, identification_card, salary, id_account) 
+VALUES ('John Doe', '1990-01-01', '1234567890', '123 Main St', 'ABC123456', 3000, 2);
+INSERT INTO service (service_name, service_price, unit) VALUES ('Grooming', 50, 1);
+INSERT INTO service (service_name, service_price, unit) VALUES ('Vaccination', 30, 1);
+INSERT INTO `status` (`status`) VALUES ('Pending');
+INSERT INTO `status` (`status`) VALUES ('Completed');
+INSERT INTO customer (customer_code, name_customer, phone_number, email, address, id_type_customer, id_account) 
+VALUES ('CUST001', 'John Doe', '1234567890', 'john.doe@example.com', '123 Main St', 1, 1);
+INSERT INTO customer (customer_code, name_customer, phone_number, email, address, id_type_customer, id_account) 
+VALUES ('CUST002', 'Jane Smith', '9876543210', 'jane.smith@example.com', '456 Elm St', 2, 2);
+INSERT INTO pet (pet_code, name_pet, weight, descriptions, id_type_pet, id_customer) 
+VALUES ('PET001', 'Max', 10, 'Friendly and playful', 1, 1);
+INSERT INTO pet (pet_code, name_pet, weight, descriptions, id_type_pet, id_customer) 
+VALUES ('PET002', 'Fluffy', 8, 'Loves to cuddle', 2, 2);
+INSERT INTO booking (booking_code, id_pet, id_employee, start_time, end_time, id_status) 
+VALUES ('BOOK001', 1, 1, '2023-11-02 10:00:00', '2023-11-02 12:00:00', 1);
+INSERT INTO booking (booking_code, id_pet, id_employee, start_time, end_time, id_status) 
+VALUES ('BOOK002', 2, 1, '2023-11-03 14:00:00', '2023-11-03 16:00:00', 2);
+INSERT INTO detail_service (id_service, id_booking, quantity) VALUES (1, 1, 1);
+INSERT INTO detail_service (id_service, id_booking, quantity) VALUES (2, 2, 1);
