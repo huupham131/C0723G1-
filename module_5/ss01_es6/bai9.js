e57ef89151e6d9c53a0107d11c9f85876c4e60bd
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-12-15 11:33:06 +0700

    Update bai9.js

--- module_5/ss01_es6/bai9.js
+++ module_5/ss01_es6/bai9.js
@@ -1,4 +1,4 @@
 // 9. Sử dụng Rest parameter để nhận vào một danh sách tên và trả về chuỗi định dạng
 // "Welcome, [tên1], [tên2], [tên3], ..." cho tất cả các tên.
-let string = (...arg)=> console.log(`Wellcome, ${arg},`)
-string("Hữu","Tưởng")
+let displayString = (...arg)=> console.log(`Wellcome, ${arg},`)
+displayString("Hữu","Tưởng")
