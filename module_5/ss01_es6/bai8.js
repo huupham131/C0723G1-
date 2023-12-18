156edd37119c5495719a3f3abd27c8a79700e02e
huupham131 <140997305+huupham131@users.noreply.github.com>
2023-12-18 10:36:56 +0700

    update ss2

--- module_5/ss01_es6/bai8.js
+++ module_5/ss01_es6/bai8.js
@@ -1,3 +1,3 @@
 // 8. Sử dụng Rest parameter và Spread operator để tạo một hàm nhận vào danh sách các số và trả về tổng của chúng.
 let sum = (...arg) =>arg.reduce((x, y) => x + y);
-console.log(sum(1,2,4,5,6))
+console.log(sum(1,2,4,5,7))
